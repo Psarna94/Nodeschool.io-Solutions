8802ada6450b96ccb597d53a6e5e072f04ff4f88
Pranu Sarna <pranusarna@gmail.com>
2015-08-28 19:25:20 +0530

    Solution to 8,9,10 programs

---
+++ learn-you-node/program09.js
@@ -0,0 +1,29 @@
+var http = require('http');
+var bl = require ('bl');
+
+http.get(process.argv[2], function(response){
+  response.pipe(bl(function(err, data){
+    if (err) return console.error(err);
+   data = data.toString();
+   console.log(data);
+ }));
+
+ http.get(process.argv[3], function(response){
+   response.pipe(bl(function(err, data){
+     if(err) return console.log(err);
+     data = data.toString();
+     console.log(data);
+   }));
+
+   http.get(process.argv[4], function(response){
+     response.pipe(bl(function(err, data){
+       if(err) return console.log(err);
+       data = data.toString();
+       console.log(data);
+     }));
+   })
+
+
+
+ });
+})
