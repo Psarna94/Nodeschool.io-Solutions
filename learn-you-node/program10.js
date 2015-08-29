c0822e4daab2f93adf1415977251384ce9bac38b
Pranu Sarna <pranusarna@gmail.com>
2015-08-29 21:10:18 +0530

    Changes in time server-learnyounode

--- learn-you-node/program10.js
+++ learn-you-node/program10.js
@@ -3,14 +3,25 @@ var d = new Date();
 var server = net.createServer(function(socket){
   var year = d.getFullYear().toString();
   var month = (d.getMonth() + 1).toString();
-  month = (month.length == 1) ? "0" + month : month;
+
+  if(month.length ==1){
+    month = '0' + month;
+  }
 
   var day = (d.getDate()).toString();
 
-  day = (day.length == 1) ? "0" + day : day;
+  if(day.length ==1){
+    day = '0'+day;
+  }
 
   var hours = d.getHours();
-  var mins = d.getMinutes();
+  if(hours.length == 1){
+    hours = '0'+hours;
+  }
+  var mins = d.getMinutes().toString();
+  if(mins.length==1){
+    mins='0'+mins;
+  }
   socket.end(year + '-' + month + '-' + day + ' ' + hours + ':' + mins + '\n');
 });
 server.listen(process.argv[2]);
